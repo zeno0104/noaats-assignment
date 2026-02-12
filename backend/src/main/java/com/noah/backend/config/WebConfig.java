630fd35054a7106523d80f776055f0c5c1a1b868
zenoo97 <jaehun4086@naver.com>
2026-02-12 22:18:44 +0900

    [ADD] 백엔드 배포 전 설정

--- backend/src/main/java/com/noah/backend/config/WebConfig.java
+++ backend/src/main/java/com/noah/backend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.noah.backend.config;
+package com.noah.backend.config; 
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/**").allowedOrigins("http://localhost:5173", "http://localhost:3000")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://*.vercel.app") // 로컬 + Vercel 모든 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
 }
